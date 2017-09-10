34c9138d28653d54d48ee14532d7e21858b631ad
rafalslaby <rafals222@outlook.com>
2017-09-10 16:40:56 +0200

    Add files via upload

---
+++ low_death max 3 divided by 25/merging_data.py
@@ -0,0 +1,22 @@
+import json
+
+data_old = open('data_old.json', 'r')
+data_new = open('data_new.json', 'r')
+
+data_old_dict = json.load(data_old)
+data_new_dict = json.load(data_new)
+
+data_old.close()
+data_new.close()
+
+y = list(data_new_dict.values())
+
+n = len(data_old_dict)
+counter = 0
+for i in y:
+    data_old_dict[n+counter] = i
+    counter+=1
+
+data_merged = open('data.json', 'w')
+json.dump(data_old_dict, data_merged)
+data_merged.close()
